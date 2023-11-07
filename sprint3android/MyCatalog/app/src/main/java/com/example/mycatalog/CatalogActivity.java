81eed149a730b385e493c5a4293a89bf6aa51a07
bilrazor <113417155+bilrazor@users.noreply.github.com>
2023-11-07 14:53:21 +0100

    update Casi completo falta comentar

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,28 +1,39 @@
 package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
-public class CatalogActivity extends AppCompatActivity {
-    Context context = this;
+public class CatalogActivity extends Fragment {
+
+    public CatalogActivity() {
+        // Constructor público vacío requerido
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.catalog_activity);
-        Button buttonOne = findViewById(R.id.buttonOne);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflar el layout para este fragmento
+        View rootView = inflater.inflate(R.layout.catalog_activity, container, false);
+
+        Button buttonOne = rootView.findViewById(R.id.buttonOne);
 
         buttonOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, DetailActivity.class);
-                context.startActivity(intent);
+                // Asegúrate de usar getActivity() para obtener el contexto de la actividad que contiene este fragmento
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(intent);
             }
         });
 
+        return rootView;
     }
 }
--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/MainActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.mycatalog;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -38,26 +39,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Fragment fragment = null;
 
         // Determinar qué elemento de menú fue seleccionado
-        switch (item.getItemId()) {
-            case R.id.nav_home: // Asegúrate de que estos IDs coincidan con los del menú del Drawer
+        int id = item.getItemId();
+        if (id == R.id.nav_home) {
+            fragment = new Fragment1();
+        } else if (id == R.id.nav_gallery) {
+            fragment = new Fragment2();
+        } else if (id == R.id.buttonOne) {
+            fragment = new CatalogActivity();
+        }
+            if (fragment == null){
                 fragment = new Fragment1();
-                break;
-            case R.id.nav_gallery:
-                fragment = new Fragment2();
-                break;
-            // Si tienes un tercer ítem en el menú, agrégalo aquí
+            }
+            // Reemplazar el fragmento actual si se seleccionó uno nuevo
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.nav_host_fragment, fragment)
+                        .commit();
+            }
+
+            // Cerrar el drawer después de seleccionar un elemento
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
         }
 
-        // Reemplazar el fragmento actual si se seleccionó uno nuevo
-        if (fragment != null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.nav_host_fragment, fragment)
-                    .commit();
-        }
-
-        // Cerrar el drawer después de seleccionar un elemento
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
